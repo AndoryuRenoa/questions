653ffbee66b5f688b766c7b97a081d8938078deb
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-01-31 11:26:56 -0700

    Update Class1.java

--- Class1.java
+++ Class1.java
@@ -1,8 +1,45 @@
+// put this in correct classes:
+int x =1 ;
 
-public class Class1 {
-  
-  int x;
-  
-  public void changeX(){
-    x++;
-  }
+public void getX(){
+return this.x;
+
+public void changeX(){
+	x++;
+}
+
+
+
+public Class2{
+
+Class1 class1 = new Class1();
+
+int a;
+
+int x = class1.getX();
+
+public assignANoParameter (){
+
+	a = class1.getX();
+
+}
+
+public assignAWithParameter (Class1 class1){
+	a = class1.getX();
+
+}
+public Class3 {
+Class1 class1 = new Class1();
+Class2 class2 = new Classs2();
+
+public void runClass1Method (){
+ 	class1.
+
+public class Main{
+public void Main (String [] args){
+	Class2 class2 = new Class2();
+	Class3 class3 = new Class3();
+	class3.runClass1Method();
+	System.out.println(class2.a);
+}
+}
