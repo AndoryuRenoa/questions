5067b264ed1c77fbfaa6c9b95f6a3870b9b7b06a
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-04-11 10:01:08 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,32 @@
 // Array Riddle
 
-
-
-class Main {
-
-Array OItem = ["a","b","c","d"]
-Array OPrice = [10.00, 5.55, 9.00, 2.00]
-Array Item = ["a", "c", "d"]
-Array Price = [10.00, 10.00, 10.00]
-
-  // figure out how to compare two arrays and get the corresponding matching values indexis so you can compare the prices
-  
-  
-  private solvePuzzle{
+public class MyClass {
+    String oItemS1[] = {"a","b","c","d"};
+    Double oPriceS1[] = {10.00, 5.55, 9.00, 2.00};
+    String itemS1[] = {"a", "c", "d"};
+    Double priceS1[] = {10.00, 10.00, 10.00};
+    int lies =0;
+    
+    private void solvePuzzle (String oItem[], Double oPrice[], String item[], Double price[]) {
+    int oItemLength = oItem.length;
+    int itemLength = item.length;
+    for (int i=0; i<oItemLength; i++){
+      for (int a=0; i<itemLength; a++){
+        if (oItem[i] == item[a]){
+          if (oPrice[i] != price[a]){
+            lies++;
+          }
+        }
+      }
+    }
   }
 
+
+    
+    public static void main(String args[]) {
+        solvePuzzle (oItemS1, oPriceS1, itemS1, priceS1);
+        System.out.println(lies);
+
+        
+    }
 }
