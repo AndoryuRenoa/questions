e73428d429d8176f38d24db8dba3c81bcbf37887
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-04-11 07:22:42 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,18 @@
-// there are no current questions in this repository
+// Array Riddle
+
+
+
 class Main {
+
+Array OItem = ["a","b","c","d"]
+Array OPrice = [10.00, 5.55, 9.00, 2.00]
+Array Item = ["a", "c", "d"]
+Array Price = [10.00, 10.00, 10.00]
+
+  // figure out how to compare two arrays and get the corresponding matching values indexis so you can compare the prices
+  
+  
+  private solvePuzzle{
+  }
+
 }
