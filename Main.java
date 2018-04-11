cba6140ee50e0916b92093aff47418514a6d7d62
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-04-11 10:11:49 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 // Array Riddle
 
 public class MyClass {
-    String oItemS1[] = {"a","b","c","d"};
-    Double oPriceS1[] = {10.00, 5.55, 9.00, 2.00};
-    String itemS1[] = {"a", "c", "d"};
-    Double priceS1[] = {10.00, 10.00, 10.00};
+    static String oItemS1[] = {"a","b","c","d"};
+    static Double oPriceS1[] = {10.00, 5.55, 9.00, 2.00};
+    static String itemS1[] = {"a", "c", "d"};
+    static Double priceS1[] = {10.00, 10.00, 10.00};
     int lies =0;
     
     private void solvePuzzle (String oItem[], Double oPrice[], String item[], Double price[]) {
@@ -12,21 +12,22 @@ public class MyClass {
     int itemLength = item.length;
     for (int i=0; i<oItemLength; i++){
       for (int a=0; i<itemLength; a++){
-        if (oItem[i] == item[a]){
+        if (oItem[i].equals(item[a])){ // silly syntax error caused all my problems
           if (oPrice[i] != price[a]){
             lies++;
           }
         }
       }
     }
+    
   }
 
 
     
     public static void main(String args[]) {
-        solvePuzzle (oItemS1, oPriceS1, itemS1, priceS1);
-        System.out.println(lies);
-
+        MyClass myClass = new MyClass();
+        myClass.solvePuzzle (oItemS1, oPriceS1, itemS1, priceS1);
+        System.out.println(myClass.lies);
         
     }
 }
