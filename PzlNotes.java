aa534a53688a068e2614e06bab26ffe623cf4eae
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-04-12 19:38:19 -0600

    Update PzlNotes.java

--- PzlNotes.java
+++ PzlNotes.java
@@ -20,3 +20,24 @@ String o1 = "(";
             
         }
         */
+
+
+
+
+
+// seperate puzzle:
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for (i2=0; i2<n; i2++){
+                
+            }
+        }
+        in.close();
+    }
+}
