8c4c580edf493bf62361ef9665fff7b049cfd4fe
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-04-12 19:15:36 -0600

    Create PzlNotes.java

---
+++ PzlNotes.java
@@ -0,0 +1,22 @@
+String o1 = "(";
+        String o2 = "{"
+        String 03 = "["
+        String c1 = ")"
+        String c2 = "}"
+        String c3 = "]"
+        String open[] = {01,02,03}
+        pass = false;
+        /*
+        for (all available opens)
+        *if (testee contains open){
+            where does testee contain open{
+                     if (testee contains corresponding close[i] at a later spot){
+                        pass= True
+                    } else{
+                     pass = flase
+                    }
+                }
+            }
+            
+        }
+        */
