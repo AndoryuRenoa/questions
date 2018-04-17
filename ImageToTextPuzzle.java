e82415b924ba8f807eba8f15a12bb1c7bf0e4238
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-04-17 08:57:03 -0600

    Update ImageToTextPuzzle.java

--- ImageToTextPuzzle.java
+++ ImageToTextPuzzle.java
@@ -1,1 +1,3 @@
 // for the purposes of this puzzle the Image containing a single text element will be 8x10 pxs
+
+// in this puzzle all text will be "black" i.e. test a value of r,g,b of less than 50,50,50 to find "black pixels"
