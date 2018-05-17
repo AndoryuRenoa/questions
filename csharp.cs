683e67ccb0f0b73273da5736a420b70ba2074a88
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-05-17 08:11:48 -0600

    Update csharp.cs

--- csharp.cs
+++ csharp.cs
@@ -1,1 +1,2 @@
 //https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/
+//good place to start here would be to create a .net framework that opens a program in c# then runs that programs macro vba code
