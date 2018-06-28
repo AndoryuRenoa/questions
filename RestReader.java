6e1589d3a0613ea57db0eb2e07e5da146506cb02
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-06-27 21:36:19 -0600

    comments to include fix to eclipse issue

--- RestReader.java
+++ RestReader.java
@@ -1,13 +1,19 @@
+package rr;
 /*
 * prior to incorporating this function in my mochijump app I will test it out standing alone first
 * could test below first, but I want to use the Jackson API to unmarshal the JSON received, that will need to be included as a dependancy
+* Eclipse is very difficult to work with regarding imporing fasterxml you can add the .jar external it's found in user/xxx/.m2/com/etcetc
 *
 */
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class RestReader {
 	
@@ -17,7 +23,6 @@ public class RestReader {
             URL url = new URL("http://mochijump.com/test/returnAll");//your url i.e fetch data from .
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
             if (conn.getResponseCode() != 200) {
                 throw new RuntimeException("Failed : HTTP Error code : "
                         + conn.getResponseCode());
